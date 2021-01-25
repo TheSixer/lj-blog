08128d33781ac78263d1bb158ba1fa65efa823bb
张龙佳 <zhanglongjia@corp.netease.com>
2021-01-25 17:51:01 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,80 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true,
+      modules: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  rules: {
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: ['.js', '.jsx', 'ts', 'tsx'],
+      },
+    ],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': [
+      0,
+      {
+        allowTypedFunctionExpressions: true,
+      },
+    ],
+    'react/state-in-constructor': 0,
+    'import/extensions': [
+      2,
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        json: 'never',
+        js: 'never',
+      },
+    ],
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'react/jsx-props-no-spreading': 0,
+    'no-unused-expressions': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'no-nested-ternary': 0,
+    'react/static-property-placement': 0,
+    'object-curly-newline': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      typescript: {
+        directory: './tsconfig.json',
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.spec.js', '**/*.spec.jsx'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
---
+++ craco.config.js
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// const CracoAntDesignPlugin = require("craco-antd");
+const CracoAlias = require('craco-alias');
+// const CracoLessPlugin = require("craco-less");
+// const CracoStylusPlugin = require('craco-stylus');
+
+// const path = require('path');
+
+module.exports = {
+  plugins: [
+    /* antd组件按需加载&样式覆盖等 */
+    // {
+    //   plugin: CracoAntDesignPlugin,
+    //   options: {
+    //     customizeThemeLessPath: path.join(
+    //       __dirname,
+    //       "src/styles/antd.theme.less"
+    //     ),
+    //   },
+    // },
+    /* 支持less module */
+    // {
+    //   plugin: CracoLessPlugin,
+    //   options: {
+    //     cssLoaderOptions: {
+    //       modules: { localIdentName: "[local]_[hash:base64:5]" },
+    //     },
+    //     modifyLessRule: function(lessRule, _context) {
+    //       lessRule.test = /\.(module)\.(less)$/;
+    //       lessRule.exclude = path.join(__dirname, 'node_modules');
+    //       return lessRule;
+    //     },
+    //   },
+    // },
+    /* 支持stylus module */
+    // {
+    //   plugin: CracoStylusPlugin,
+    //   options: {
+    //     cssLoaderOptions: {
+    //       modules: { localIdentName: "[local]_[hash:base64:5]" },
+    //     },
+    //     modifyStylusRule: function (stylRule, _context) {
+    //       stylRule.test = /\.(module)\.(styl)$/;
+    //       stylRule.exclude = path.join(__dirname, 'node_modules');
+    //       return stylRule;
+    //     },
+    //   },
+    // },
+    /* 别名设置 */
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './src',
+        tsConfigPath: './tsconfig.extend.json',
+      },
+    },
+  ],
+  devServer: (devServerConfig) => {
+    return {
+      ...devServerConfig,
+      // 服务开启gzip
+      compress: true,
+      proxy: {
+        '/api': {
+          target: 'https://test.com/',
+          changeOrigin: true,
+          xfwd: false,
+        },
+      },
+    };
+  },
+};
