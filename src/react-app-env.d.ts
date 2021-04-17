3ca394e3f50634b1dc69d44dc9e43f0aeaaa3f35
张龙佳 <zhanglongjia@corp.netease.com>
2021-04-17 21:19:53 +0800

    add code highlight for markdown

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -6,3 +6,23 @@ declare global {
     export = styles;
   }
 }
+
+declare module 'highlight.js/lib/core';
+declare module 'highlight.js/lib/languages/go';
+declare module 'highlight.js/lib/languages/css';
+declare module 'highlight.js/lib/languages/sql';
+declare module 'highlight.js/lib/languages/php';
+declare module 'highlight.js/lib/languages/xml';
+declare module 'highlight.js/lib/languages/yaml';
+declare module 'highlight.js/lib/languages/json';
+declare module 'highlight.js/lib/languages/bash';
+declare module 'highlight.js/lib/languages/less';
+declare module 'highlight.js/lib/languages/scss';
+declare module 'highlight.js/lib/languages/rust';
+declare module 'highlight.js/lib/languages/java';
+declare module 'highlight.js/lib/languages/shell';
+declare module 'highlight.js/lib/languages/nginx';
+declare module 'highlight.js/lib/languages/stylus';
+declare module 'highlight.js/lib/languages/python';
+declare module 'highlight.js/lib/languages/javascript';
+declare module 'highlight.js/lib/languages/typescript';
